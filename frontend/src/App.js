3515898f21b49f64de2f07637ec7c90e5ef1c6fb
alonyafhe7 <alonyafhe7@gmail.com>
2026-03-02 19:57:18 +0200

    Create FarmersMarket React component with Hebrew text

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FarmersMarket = () => {
+    return (
+        <div>
+            <h1>שוק החקלאים</h1>
+            <p>ברוכים הבאים לשוק החקלאים שלנו!</p>
+        </div>
+    );
+};
+
+export default FarmersMarket;
